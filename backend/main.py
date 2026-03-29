f6a8782ad16a1838afe2a4e158c8d18583fafabe
Tirth <tirth11804@gmail.com>
2026-03-29 17:30:28 -0400

    fixed a little error with the cors middleware

--- backend/main.py
+++ backend/main.py
@@ -7,8 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
